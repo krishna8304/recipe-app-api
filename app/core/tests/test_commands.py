e0083832c237c73d2c76be79940f14a79f0c6709
krishna8304 <krishnabonthu134@gmail.com>
2024-03-13 13:06:36 +0530

    Updated code with database variables

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,6 +10,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands"""
@@ -26,8 +27,8 @@ class CommandTests(SimpleTestCase):
         """Test waiting for db whenb getting Operational Error"""
         patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
-        
+
         call_command('wait_for_db')
-        
+
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
