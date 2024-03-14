a812800535167353373ec54aedf8b1da3581997b
krishna8304 <krishnabonthu134@gmail.com>
2024-03-14 20:31:21 +0530

    Update with user api new

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,30 @@
+"""
+Tests for Models.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """ Tests Models """
+    def test_create_user_with_email_successfull(self):
+        """Test creating a user with an email is successful"""
+        email = 'test@gmail.com'
+        password = 'test123'
+        user = get_user_model().objects.create_user(email=email,
+                                                    password=password)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users"""
+        sample_emails = [
+            ['test1@example.com', 'test1@example.com'],
+            ['Test2@example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+            ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email,
+                                                        'sample123')
+            self.assertEqual(user.email, expected)
